0fc7445a71db26cda601d96b1977286352bcc6e3
hanccy <31921126+hanccy@users.noreply.github.com>
2025-04-20 00:15:08 +0800

    Update main.py

--- main.py
+++ main.py
@@ -10,9 +10,9 @@ import time
 import requests
 
 # 开启根据地区天气情况降低步数（默认关闭）
-open_get_weather = TRUE 
+open_get_weather = sys.argv[3]
 # 设置获取天气的地区（上面开启后必填）如：area = "宁波"
-area = "郑州"
+area = sys.argv[4]
 
 # 以下如果看不懂直接默认就行只需改上面
 
